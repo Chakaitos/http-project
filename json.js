4c55471e773d6da09aa52d88514a76958c5c6d0d
Jose R. Borja <chakaitos@gmail.com>
2014-03-21 15:01:34 -0500

    Add sample json data file

---
+++ json.js
@@ -0,0 +1,44 @@
+[    
+    {
+        "firstName": "Carla",
+        "lastName": "Guzman",
+        "age": 28,
+        "address": {
+            "streetAddress": "4711 Sunrise Ave.",
+            "city": "Fort Lauderdale",
+            "state": "FL",
+            "postalCode": 33305
+        },
+        "phoneNumbers": [
+            {
+                "type": "cell",
+                "number": "787 672-1234"
+            },
+            {
+                "type": "work",
+                "number": "954 555-4567"
+            }
+        ]
+    },
+    {
+        "firstName": "Sara",
+        "lastName": "Guerrero",
+        "age": 48,
+        "address": {
+            "streetAddress": "10285 Kendall Dr.",
+            "city": "Miami",
+            "state": "FL",
+            "postalCode": 33186
+        },
+        "phoneNumbers": [
+            {
+                "type": "cell",
+                "number": "305 672-1234"
+            },
+            {
+                "type": "work",
+                "number": "786 555-4567"
+            }
+        ]
+    }
+]
